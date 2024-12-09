6c4e1f0380f44b0f913042eb7e3cacaf796f5905
roman deblase <rdeblase2000@gmail.com>
2024-12-08 22:53:35 -0500

    please

--- src/components/ChartComponent.jsx
+++ src/components/ChartComponent.jsx
@@ -1,27 +1,25 @@
+
 import React, { useEffect, useRef } from 'react';
 import { Chart } from 'ChartComponent.jsx';
 
-const ChartComponent = ({ type, data, options }) => {
-  const chartRef = useRef(null);
+const ChartComponent = ({type, data, options }) => {
+    const chartRef = useRef(null);
 
-  useEffect(() => {
-    if (chartRef.current) {
-      new Chart(chartRef.current, {
-        type: type,
-        data: data,
-        options: options,
-      });
-    }
-
-    // Cleanup chart instance on component unmount
-    return () => {
-      if (chartRef.current) {
-        chartRef.current.destroy();
-      }
-    };
-  }, [data, options, type]);
-
-  return <canvas ref={chartRef}></canvas>;
+    useEffect(() => => {
+        if (chartRef.current) {
+            new Chart(chartRef.current, {
+                type: type,
+                data: data,
+                options: options,
+            });
+        }
+        return () => {
+            if (chartRef.current) {
+                chartRef.current.destroy();
+            }
+        };
+    }, [data, options, type]);
+    return <canvas ref={chartRef}></canvas>
 };
 
 export default ChartComponent;
